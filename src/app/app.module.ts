71887a443d057c47abc6333d07a5011639e76279
VladyslavShmykov <vladislavshmuk@gmail.com>
2021-07-26 15:50:54 +0300

    User + post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { SingleUserComponent } from './single-user/single-user.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './posts/posts.component';
+import { SinglePostsComponent } from './single-posts/single-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    SingleUserComponent,
+    PostsComponent,
+    SinglePostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
